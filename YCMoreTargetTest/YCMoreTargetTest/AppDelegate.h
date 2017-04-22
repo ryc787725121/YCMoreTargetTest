29c1768931e14ca3985f7be3f4d74283a0faf012
renyichun <787725121@qq.com>
2017-04-22 18:23:43 +0800

    初始化项目

---
+++ YCMoreTargetTest/YCMoreTargetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YCMoreTargetTest
+//
+//  Created by 任任义春 on 2017/4/22.
+//  Copyright © 2017年 renyichun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YCMoreTargetTest/YCMoreTargetTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YCMoreTargetTest
+//
+//  Created by 任任义春 on 2017/4/22.
+//  Copyright © 2017年 renyichun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
