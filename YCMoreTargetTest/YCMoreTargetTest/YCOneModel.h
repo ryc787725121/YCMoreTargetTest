48e4557d67ea2a7c521d126c58ee90bb0aa4084a
renyichun <787725121@qq.com>
2017-04-22 18:32:36 +0800

    多个Target

---
+++ YCMoreTargetTest/YCMoreTargetTest/YCOneModel.h
@@ -0,0 +1,13 @@
+//
+//  YCOneModel.h
+//  YCMoreTargetTest
+//
+//  Created by 任任义春 on 2017/4/22.
+//  Copyright © 2017年 renyichun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YCOneModel : NSObject
+
+@end
