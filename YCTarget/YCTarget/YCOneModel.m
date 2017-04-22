d7005c576a7b07607a3e48125b9132dcf005839d
renyichun <787725121@qq.com>
2017-04-22 18:50:00 +0800

    第一个创建model

---
+++ YCTarget/YCTarget/YCOneModel.m
@@ -0,0 +1,13 @@
+//
+//  YCOneModel.m
+//  YCTarget
+//
+//  Created by 任任义春 on 2017/4/22.
+//  Copyright © 2017年 renyichun. All rights reserved.
+//
+
+#import "YCOneModel.h"
+
+@implementation YCOneModel
+
+@end
